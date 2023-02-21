5c390f3a1fdc6191192bc9291be43a15397de6c8
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-02-21 12:05:54 +0300

    feat: adding the task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
+    left;
+    right;
+  
+    constructor() {
+    }
+  
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
-
+  
     guess() {
-
+        return this.middle();
     }
-
+  
     lower() {
-
+        this.right = this.middle();
     }
-
+  
     greater() {
-
+        this.left = this.middle();
     }
-}
+  
+    middle() {
+        return Math.round((this.right - this.left) / 2) + this.left;
+    }
+  }
 
 module.exports = GuessingGame;
